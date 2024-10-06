7b67707ba158e13a55a8514782a07ccae15296f8
Raul Resendez <raul.resendez@proton.me>
2024-10-06 02:00:47 -0500

    Ongoing issues with logic

--- javascript.js
+++ javascript.js
@@ -1,99 +1,92 @@
-
 /* 
 Gameboard represents the current state of the tic-tac-toe board
 Each square is a cell that can hold a player's shape (X or O)
  */
 
+function gameBoard() {
+  //initiaizing board
+  const board = {};
 
-function gameBoard(){
-
-
-    //initiaizing board
-    const board = {};
-
-    //populating empty ttt board
-    for (let row = 0; row < 3; row++){
-        for (let column = 0; column < 3; column++){
-            board[`${row}, ${column}`] = null;
-        }
+  //populating empty ttt board
+  for (let row = 0; row < 3; row++) {
+    for (let column = 0; column < 3; column++) {
+      board[`${row}, ${column}`] = null;
     }
+  }
 
-    const resetBoard = () =>{
-        for (let row = 0; row < 3; row++){
-            for (let column = 0; column < 3; column++){
-                board[`${row}, ${column}`] = null;
-            }
-        }
-
+  const resetBoard = () => {
+    for (let row = 0; row < 3; row++) {
+      for (let column = 0; column < 3; column++) {
+        board[`${row}, ${column}`] = null;
+      }
     }
+  };
 
+  // return current board
+  const getBoard = () => board;
 
+  const printBoard = () => console.log(board);
 
-    // return current board
-    const getBoard = ()=>board;
+  const addShape = (playerShape, row, col) => {
+    const key = `${row},${col}`;
 
-    
-
-    const addShape = (playerShape, row, col) =>{
-
-        const key = `${row},${col}`;
-        
-        if (board[key] === null){
-            board[key] = playerShape;
-        }
-        else {
-            console.log("Cell already Occupied");
-        }
-
-
-        
-
+    if (board[key] === null) {
+      board[key] = playerShape;
+    } else {
+      console.log("Cell already Occupied");
     }
+  };
 
-    return{
-        getBoard,
-        addShape,
-        resetBoard
-    };
-    
-
-
-
-    
+  return {
+    getBoard,
+    addShape,
+    resetBoard,
+    printBoard,
+  };
 }
 
-const createPlayer = (name, shape, playerTurn)=>{
-    let score = 0;
-    const increaseScore = () => score++;
-    const getScore = () => score;
-    const getPlayerTurn = () => playerTurn;
-    const changePlayerTurn = () => playerTurn != playerTurn;
+const createPlayer = (name, shape, playerTurn) => {
+  let score = 0;
+  const increaseScore = () => score++;
+  const getScore = () => score;
+  const getPlayerTurn = () => playerTurn;
+  const changePlayerTurn = () => playerTurn != playerTurn;
 
-    return {
-        name,
-        shape,
-        getScore,
-        increaseScore,
-        getPlayerTurn,
-        changePlayerTurn
-    };
+  return {
+    name,
+    shape,
+    getScore,
+    increaseScore,
+    getPlayerTurn,
+    changePlayerTurn,
+  };
+};
 
-}
+const gameController = () => {
+  const gameboard = gameBoard();
 
-const gameController = (function(){
+  //   const resetBoard = () => gameboard.resetBoard();
+  const playerOne = createPlayer("Player One", "O", true);
+  const playerTwo = createPlayer("player Two", "X", false);
+  let currentPlayer = playerOne;
+  const playRound = () => {
+    gameboard.printBoard();
 
-    const gameboard = gameBoard();
+    const playerRowInput = prompt(
+      `${currentPlayer.name}'s Turn.\n Please Input your choice of Row:`
+    );
+    const playerColInput = prompt(`Please Input your choice of Column:`);
 
-    const resetBoard = ()=>gameboard.resetBoard();
+    gameboard.addShape(currentPlayer.shape, playerRowInput, playerColInput);
 
-    const playRound = (playerOneName, playerTwoName) =>{
-        const playerOne =createPlayer(playerOneName, 'O', true);
-        const playerTwo = createPlayer(playerTwoName, 'X', false);
+    // Switch turn
+    currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+  };
 
-        
+  return {
+    playRound,
+  };
+};
 
-
-    }
-
-    
-})
+const myGameController = gameController();
+myGameController.playRound();
